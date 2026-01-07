1e5884fc543596e278b1f6fabedc5c1106659861
aknandan sharma <aknandan.kumar@decorpot.com>
2026-01-07 15:59:37 +0530

    knfka

--- NODE_01_2026/index.js
+++ NODE_01_2026/index.js
@@ -1,1 +1,25 @@
-console.log("This is NODE_01_2026/index.js");
+console.log("This is JavaScript Revision.");
+
+// This is Object in js.
+
+// This is Type 1: U
+let object = {
+    name: "AKnandan Sharma",
+    age: 24,
+    class: "BCA",
+    College: "GAYA College, Gaya"
+}
+
+let obj1 = new Object();
+obj1.name = "Akash Sharma";
+obj1.age =  "25th";
+obj1.address = "Gaya, BIHAR"
+
+function objectDetaiils(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+let fak = new objectDetaiils("aknandan", 27);
+console.table(fak)
+
